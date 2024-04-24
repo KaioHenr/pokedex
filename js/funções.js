5f0146f90c00da7a1ba20cbc0aaed5aa1c3e4a2e
KaioHenr <120226915+KaioHenr@users.noreply.github.com>
2024-04-23 23:22:43 -0300

    finalização do projeto

--- js/funções.js
+++ js/funções.js
@@ -1,3 +1,23 @@
+const colors = {
+    normal: "#a8a77a",
+    fire: "#ee8130",
+    water: "#6390f0",
+    electric: "#f7d02c",
+    grass: "#7ac74c",
+    ice: "#96d9d6",
+    fighting: "#c22e28",
+    poison: "#a33ea1",
+    ground: "#e2bf65",
+    flying: "#a98ff3",
+    psychic: "#f95587",
+    bug: "#a6b91a",
+    rock: "#b6a136",
+    ghost: "#735797",
+    dragon: "#6f35fc",
+    dark: "#705746",
+    steel: "#b7b7ce",
+    fairy: "#d685ad",
+};
 async function buscaInfo(id) {
     let pokeExiste = JSON.parse(localStorage.getItem(id));
     if (pokeExiste != null) {
@@ -88,6 +108,9 @@ async function ComparaRecebe1() {
     }
     await Swal.fire({
         title: "Selecione o Pokémon para comparar com o " + pokePesquisado.nome,
+        html:"<div style='display: flex; justify-content: space-around; align-items: center;'><img src='" +
+        pokePesquisado.img +
+        "'/> <img src='../img/vs.svg'/> <img src='../img/interrogacao.svg' style='width: 100px;'/></div>",
         input: "select",
         inputOptions: pokeListaNomes,
         showCancelButton: true,
@@ -96,31 +119,58 @@ async function ComparaRecebe1() {
             pokeListaNomes[segundoResult.value]
         );
         if (pokePesquisado.id == pokeSelecionado.id) {
-            Swal.fire("Escolheu o mesmo pokemon!!");
+            Swal.fire({
+                title: "Você escolheu o mesmo Pokémon!!!",
+                html:
+                    "<div style='display: flex; justify-content: space-around; align-items: center;'><img src='" +
+                    pokePesquisado.img +
+                    "'/> <img src='../img/vs.svg'/> <div style='display: flex; justify-content: space-around; align-items: center;'><img src='" +
+                    pokeSelecionado.img +
+                    "'/>",
+            });
         } else if (pokePesquisado.base_stats > pokeSelecionado.base_stats) {
-            Swal.fire(
-                pokePesquisado.nome +
-                    " e mais forte com " +
+            Swal.fire({
+                title: pokePesquisado.nome + " e mais forte💪💪",
+                html:
+                    "<div style='display: flex; justify-content: center;flex-direction: column;'><div style='display: flex;gap: 20px;'><img src='" +
+                    pokePesquisado.img +
+                    "' alt='' style = 'width: 150px;' /><img src='../img/maior-que.svg' alt='sinal' style = 'width: 100px;' /><img src='" +
+                    pokeSelecionado.img +
+                    "' alt='' style = 'width: 150px;' /></div><h1>" +
                     parseInt(
-                        pokePesquisado.base_stats - pokeSelecionado.base_stats
+                        pokePesquisado.base_stats -
+                        pokeSelecionado.base_stats
                     ) +
-                    " pontos de status a mais!!"
-            );
+                    " pontos de status a mais!!</h1></div>",
+            });
         } else if (pokePesquisado.base_stats == pokeSelecionado.base_stats) {
-            Swal.fire(
+            Swal.fire({
+                title:
                 pokePesquisado.nome +
                     " tem a mesma quantidade de status que " +
-                    pokeSelecionado.nome
-            );
+                    pokeSelecionado.nome,
+                html:
+                    "<div style='display: flex; justify-content: center;flex-direction: column;'><div style='display: flex;gap: 20px;'><img src='" +
+                    pokePesquisado.img +
+                    "' alt='' style = 'width: 150px;' /><img src='../img/igual.svg' alt='sinal' style = 'width: 100px;' /><img src='" +
+                    pokeSelecionado.img +
+                    "' alt='' style = 'width: 150px;' /></div></div>",
+            });
         } else {
-            Swal.fire(
-                pokeSelecionado.nome +
-                    " e mais forte com " +
+            Swal.fire({
+                title: pokeSelecionado.nome + " e mais forte💪💪",
+                html:
+                    "<div style='display: flex; justify-content: center;flex-direction: column;'><div style='display: flex;gap: 20px;'><img src='" +
+                    pokeSelecionado.img +
+                    "' alt='' style = 'width: 150px;' /><img src='../img/maior-que.svg' alt='sinal' style = 'width: 100px;' /><img src='" +
+                    pokePesquisado.img +
+                    "' alt='' style = 'width: 150px;' /></div><h1>" +
                     parseInt(
-                        pokeSelecionado.base_stats - pokePesquisado.base_stats
-                    ) +
-                    " pontos de status a mais!!"
-            );
+                        pokePesquisado.base_stats -
+                        pokeSelecionado.base_stats
+                    )*(-1) +
+                    " pontos de status a mais!!</h1></div>",
+            });
         }
     });
 }
@@ -165,37 +215,63 @@ async function comparaRecebe2() {
                         pokeListaNomes[segundoResult.value]
                     );
                     if (primeiroPokemon.id == segundoPokemon.id) {
-                        Swal.fire("Escolheu o mesmo pokemon!!");
+                        Swal.fire({
+                            title: "Você escolheu o mesmo Pokémon!!!",
+                            html:
+                                "<div style='display: flex; justify-content: space-around; align-items: center;'><img src='" +
+                                primeiroPokemon.img +
+                                "'/> <img src='../img/vs.svg'/> <div style='display: flex; justify-content: space-around; align-items: center;'><img src='" +
+                                segundoPokemon.img +
+                                "'/>",
+                        });
                     } else if (
                         primeiroPokemon.base_stats > segundoPokemon.base_stats
                     ) {
-                        Swal.fire(
-                            primeiroPokemon.nome +
-                                " e mais forte com " +
+                        Swal.fire({
+                            title: primeiroPokemon.nome + " e mais forte💪💪",
+                            html:
+                                "<div style='display: flex; justify-content: center;flex-direction: column;'><div style='display: flex;gap: 20px;'><img src='" +
+                                primeiroPokemon.img +
+                                "' alt='' style = 'width: 150px;' /><img src='../img/maior-que.svg' alt='sinal' style = 'width: 100px;' /><img src='" +
+                                segundoPokemon.img +
+                                "' alt='' style = 'width: 150px;' /></div><h1>" +
                                 parseInt(
                                     primeiroPokemon.base_stats -
                                         segundoPokemon.base_stats
                                 ) +
-                                " pontos de status a mais!!"
-                        );
+                                " pontos de status a mais!!</h1></div>",
+                        });
                     } else if (
                         primeiroPokemon.base_stats == segundoPokemon.base_stats
                     ) {
-                        Swal.fire(
-                            primeiroPokemon.nome +
+                        Swal.fire({
+                            title:
+                                primeiroPokemon.nome +
                                 " tem a mesma quantidade de status que " +
-                                segundoPokemon.nome
-                        );
+                                segundoPokemon.nome,
+                            html:
+                                "<div style='display: flex; justify-content: center;flex-direction: column;'><div style='display: flex;gap: 20px;'><img src='" +
+                                segundoPokemon.img +
+                                "' alt='' style = 'width: 150px;' /><img src='../img/igual.svg' alt='sinal' style = 'width: 100px;' /><img src='" +
+                                primeiroPokemon.img +
+                                "' alt='' style = 'width: 150px;' /></div></div>",
+                        });
                     } else {
-                        Swal.fire(
-                            segundoPokemon.nome +
-                                " e mais forte com " +
+                        Swal.fire({
+                            title: segundoPokemon.nome + " e mais forte💪💪",
+                            html:
+                                "<div style='display: flex; justify-content: center;flex-direction: column;'><div style='display: flex;gap: 20px;'><img src='" +
+                                segundoPokemon.img +
+                                "' alt='' style = 'width: 150px;' /><img src='../img/maior-que.svg' alt='sinal' style = 'width: 100px;' /><img src='" +
+                                primeiroPokemon.img +
+                                "' alt='' style = 'width: 150px;' /></div><h1>" +
                                 parseInt(
-                                    segundoPokemon.base_stats -
-                                        primeiroPokemon.base_stats
-                                ) +
-                                " pontos de status a mais!!"
-                        );
+                                    primeiroPokemon.base_stats -
+                                        segundoPokemon.base_stats
+                                ) *
+                                    -1 +
+                                " pontos de status a mais!!</h1></div>",
+                        });
                     }
                 }
             });
